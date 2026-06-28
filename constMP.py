2dd7a046327338b92b5b8f22d53773bc4dc6be95
Samuel Amorim <68171967+esqzerolol@users.noreply.github.com>
2026-06-28 13:16:44 -0300

    Update constMP.py

--- constMP.py
+++ constMP.py
@@ -1,12 +1,17 @@
-SERVICE_NAMES_ADDR = "44.203.222.64"
-SERVICE_NAMES_TCP_PORT = 5678
+import os
+
+SERVICE_NAMES_ADDR = os.getenv("SERVICE_NAMES_ADDR", "127.0.0.1")
+SERVICE_NAMES_TCP_PORT = int(os.getenv("SERVICE_NAMES_TCP_PORT", "5678"))
 
 SERVER_NAME = "comparisonServer"
 SERVER_TYPE = "server"
 PEER_TYPE = "peer"
 
-SERVER_PORT = 5679
-PEER_TCP_PORT = 5680
-PEER_UDP_PORT = 5680
+SERVER_PORT = int(os.getenv("SERVER_PORT", "5679"))
+PEER_TCP_PORT = int(os.getenv("PEER_TCP_PORT", "5680"))
+PEER_UDP_PORT = int(os.getenv("PEER_UDP_PORT", "5680"))
+
+ADVERTISE_ADDR = os.getenv("ADVERTISE_ADDR")
+MIN_PEERS = int(os.getenv("MIN_PEERS", "6"))
 
 DB_FILE_PREFIX = "replica_db_peer_"
